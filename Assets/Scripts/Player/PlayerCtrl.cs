ad36097592dd3eb585b58012cee9b67cdcbddca2
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-03-18 01:07:34 +0800

    add

--- Assets/Scripts/Player/PlayerCtrl.cs
+++ Assets/Scripts/Player/PlayerCtrl.cs
@@ -3,51 +3,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    private PlayerInput playerInput;
+
+    private Vector2 mousePosition;
+
     private void Awake()
     {
-        var playerInput = GetComponent<PlayerInput>();
-        if (playerInput != null)
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInput.actions["Move"].performed += MoveCallback;
+        playerInput.actions["QWER"].performed += QWERCallback;
+        playerInput.actions["Number"].performed += NumberCallback;
+    }
+
+    private void OnEnable()
+    {
+        playerInput.actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerInput.actions.Disable();
+    }
+
+    private void Update()
+    {
+        if(Mouse.current.leftButton.isPressed)
         {
-            playerInput.onActionTriggered += MoveCallback;
-            playerInput.onActionTriggered += QWERCallback;
-            playerInput.onActionTriggered += NumberCallback;
+            Debug.Log(
+            Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()));
         }
     }
 
     private void MoveCallback(InputAction.CallbackContext ctx)
     {
-        
         if (ctx.control == Mouse.current.leftButton)
         {
-            Debug.Log(1);
+            if (ctx.interaction is PressInteraction interaction)
+            {
+                if (interaction.behavior == PressBehavior.PressOnly)
+                {
+                    Debug.Log(212);
+                }
+                else if (interaction.behavior == PressBehavior.ReleaseOnly)
+                {
+                    Debug.Log(333);
+                }
+            }
+
         }
         else if (ctx.control == Mouse.current.rightButton)
         {
-            Debug.Log(2);
+            if (ctx.interaction is PressInteraction)
+            {
+
+            }
+            else if (ctx.interaction is TapInteraction)
+            {
+
+            }
         }
+
     }
+
 
     private void QWERCallback(InputAction.CallbackContext ctx)
     {
         if (ctx.control == Keyboard.current.qKey)
         {
-            Debug.Log(1);
+            Debug.Log('Q');
         }
         else if (ctx.control == Keyboard.current.wKey)
         {
-            Debug.Log(1);
+            Debug.Log('W');
         }
         else if (ctx.control == Keyboard.current.eKey)
         {
-            Debug.Log(1);
+            Debug.Log('E');
         }
         else if (ctx.control == Keyboard.current.rKey)
         {
-            Debug.Log(1);
+            Debug.Log('R');
         }
     }
 
@@ -57,6 +97,17 @@ public class PlayerCtrl : MonoBehaviour
         {
             Debug.Log(1);
         }
+        else if (ctx.control == Keyboard.current.digit2Key)
+        {
+            Debug.Log(2);
+        }
+        else if (ctx.control == Keyboard.current.digit3Key)
+        {
+            Debug.Log(3);
+        }
+        else if (ctx.control == Keyboard.current.digit4Key)
+        {
+            Debug.Log(4);
+        }
     }
-
 }
