2628a3c16276a27972eb213efb3a76a56a69228d
HHHHHHHHHHHHHHHHHHHHHCS <464962683@qq.com>
2020-03-24 22:22:55 +0800

    add

---
+++ Assets/Scripts/UI/MainUIManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class MainUIManager : MonoBehaviour
+{
+    private static MainUIManager _instance;
+
+    public static MainUIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = FindObjectOfType<MainUIManager>();
+            return _instance;
+        }
+    }
+
+    public AssetReference TargetToLoad;
+
+
+    private void Awake()
+    {
+        _instance = this;
+
+        LoadMethod();
+    }
+
+    private void OnDisable()
+    {
+        LoadMethod2();
+    }
+
+    public async void LoadMethod()
+    {
+        var v0 = await Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/UI/TalkWidow.prefab").Task;
+        //TargetToLoad.InstantiateAsync().Completed += OnInstantiatedCompleted;
+        Addressables.Release(v0);
+
+        var v1 = await Addressables.InstantiateAsync("Assets/Prefabs/UI/TalkWidow.prefab").Task;
+        //TargetToLoad.InstantiateAsync().Completed += OnInstantiatedCompleted;
+        Addressables.Release(v1);
+    }
+
+    public async void LoadMethod2()
+    {
+        var instance = await Addressables.LoadAssetAsync<GameObject>("Assets/Prefabs/Human/Player.prefab").Task;
+        Addressables.Release(instance);
+    }
+}
---
+++ Assets/Scripts/UI/UIWindowMonoBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIWindowMonoBase
+{
+    protected MainUIManager mainUIManager;
+
+
+    public virtual void OnCtor(MainUIManager _uiManager)
+    {
+        mainUIManager = _uiManager;
+    }
+
+    public virtual void OnAwake()
+    {
+
+    }
+
+    public virtual void OnEnable()
+    {
+
+    }
+
+    public virtual void OnDisable()
+    {
+
+    }
+
+    public virtual void OnUpdate()
+    {
+
+    }
+}
